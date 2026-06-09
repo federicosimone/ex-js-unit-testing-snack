d18ead729d93d68b04610e5302584494586c9053
federicosimone <federico.simone15@gmail.com>
2026-06-09 23:05:28 +0200

    snack 2 e 3

---
+++ test.js
@@ -0,0 +1,36 @@
+// snack 1 
+
+function getInitials(name) {
+    const parts = name.split(" ")
+    return parts[0][0] + parts[1][0];
+}
+
+
+test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
+    expect(getInitials("Federico Simone")).toBe("FS")
+})
+
+//snack 2 
+
+function createSlug(string) {
+
+    return string.toLowerCase()
+}
+
+test("La funzione createSlug restituisce una stringa in lowercase", () => {
+    expect(createSlug("MAIUSCOLO")).toBe("maiuscolo")
+})
+
+//snack 3
+
+function average(array) {
+
+    const somma = array.reduce((acc, num) => acc + num, 0)
+    return somma / array.length
+}
+
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+    expect(average([1, 2, 3, 4, 5])).toBe(3)
+})
+
+//snack 
